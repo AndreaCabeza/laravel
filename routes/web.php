4787fc7e35f7480a07feaee4199e6c6bc11bb07b
Andrea Cabeza <andreiitaandyy@gmail.com>
2023-07-16 18:30:27 -0500

    ok

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/restaurante', function () {
+    $restaurante= [
+        ["name" => "Para compartir"],
+        ["name" => "Individual"],
+        ["name" => "Familiar"],
+    ];
+    return view('menú', ["restaurante" =>$restaurante]);
+})->name ("restaurante");
+
+Route::get('/restaurante', [ProductoController::class, "index"]) ->name("restaurante.index");
